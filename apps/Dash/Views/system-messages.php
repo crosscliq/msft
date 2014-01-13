82bc01d5de560fa62b74de37c8a96d7fdfcf04ea
root <root@msft.cc>
2014-01-13 18:22:15 +0000

    anything on live

--- apps/Dash/Views/system-messages.php
+++ apps/Dash/Views/system-messages.php
@@ -1,1 +1,5 @@
-<?php echo \Dsc\System::instance()->renderMessages(); ?>
+<div class="alert alert-success alert-block fade in">
+                    <button type="button" class="close close-sm" data-dismiss="alert"> <i class="icon-remove"></i> </button>
+                    <h4> <i class="icon-ok-sign"></i> Success! </h4>
+                    <p><?php echo \Dsc\System::instance()->renderMessages(); ?></p>
+                  </div>
