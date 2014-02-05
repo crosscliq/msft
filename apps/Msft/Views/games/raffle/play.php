bdd375dfbe1ad0ae905463d9472f9db000498f43
root <root@msft.cc>
2014-02-05 19:51:00 +0000

    everything

--- apps/Msft/Views/games/raffle/play.php
+++ apps/Msft/Views/games/raffle/play.php
@@ -12,8 +12,11 @@
                 <div id="role" style="text-align:center;">
 			
 			<h2 class="fg-white"><?php echo $game["winner"]["first_name"]; ?> <?php echo $game["winner"]["last_name"]; ?></h2>
-			
-
+			<br/><br/>
+<form action="/games/raffle/play" method="post">
+<button type="submit"  class="button large inverse fg-white" style="width:80%; margin-bottom:25px;background:rgba(0,0,0,0.6)!important">Play Again</button>
+</form>
+		
                 </div>
 
 
