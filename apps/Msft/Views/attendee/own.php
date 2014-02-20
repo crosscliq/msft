8782c7180c66349907469edde67d41886c252570
root <root@msft.cc>
2014-02-20 18:30:18 +0000

    everything

--- apps/Msft/Views/attendee/own.php
+++ apps/Msft/Views/attendee/own.php
@@ -4,9 +4,9 @@
     <div class="grid">
       <div class="row">
         <div class="span12" style="text-align:center;">
-           <h1 class="fg-white"><?php echo $SESSION['event']->name;?></h1>
+           <h1 class="fg-white"><?php echo @$SESSION['event']->name;?></h1>
 		<hr/>
-            <h2 class="fg-white"><?php echo $SESSION['event']->dates['start_date'];?></h2>
+            <h2 class="fg-white"><?php echo @$SESSION['event']->dates['start_date'];?></h2>
         </div>
       </div>
     </div>
