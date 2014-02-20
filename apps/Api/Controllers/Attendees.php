7192d799d520726ab99e6515817c15b064920d53
Chris French <chris@ammonitenetworks.com>
2014-02-20 11:09:13 -0800

    adding API

---
+++ apps/Api/Controllers/Attendees.php
@@ -0,0 +1,31 @@
+<?php 
+namespace Api\Controllers;
+
+class Attendees extends \Dsc\Controller  
+{	
+
+	
+     public function Sync($f3)
+    {
+
+        $body = $f3->get('BODY');
+
+        $object = json_decode($body);
+
+        //get the EVENT ID FROM URL
+        $f3->set('event.db',explode('.',parse_url( $object->Url, PHP_URL_HOST ))[0]);
+
+        $model = new \Api\Models\Attendees;
+
+        $model->create((array) $object );
+        
+        $result = array('response' => true, 'msg' => 'Attendee Saved');
+        echo json_encode($result);
+        exit();
+
+    }
+	
+ 
+
+}
+?> 
