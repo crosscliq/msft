7e676cb95e56d37995b7124575e7df84234662c3
Chris French <chris@ammonitenetworks.com>
2014-02-27 16:32:25 -0700

    testing stuff

--- apps/Api/Controllers/Attendees.php
+++ apps/Api/Controllers/Attendees.php
@@ -4,6 +4,32 @@ namespace Api\Controllers;
 class Attendees extends \Dsc\Controller  
 {	
 
+/*{
+  "UUID": "9e1ab126-0835-4cbd-ae1a-4e79172988d3",
+  "Url": "http://ontario.msft.cc/band/onice50",
+  "StaffNumber": "abc",
+  "first_name": "Test",
+  "last_name": "Test",
+  "email": "test234@test.com",
+  "phone": "2580",
+  "zipcode": "4567",
+  "products": {
+    "xboxone": "on",
+    "xbox360": "on",
+    "kinect": "on",
+    "surface": "on",
+    "pc": "on",
+    "windowsphone": "on",
+    "office": "on"
+  },
+  "howdidyouhear": "Newspaper",
+  "offers": {
+    "email": "on",
+    "sms": "on"
+  },
+  "RegisterDate": "2014-02-27T10:40:29.136137-07:00"
+}*/
+
 
 	
      public function Sync($f3)
@@ -19,11 +45,19 @@ class Attendees extends \Dsc\Controller
 
         $model = new \Api\Models\Attendees;
 
+        $url = parse_url($object->Url);
+        
+        $model = new \Msft\Models\Tags;
+    
+        $tag = $model->getPrefab();
+        $tag->tagid = end(explode('/',parse_url($object->Url)));
+        $tag->eventid = $f3->get('event.db');
+        $newTag = $model->create((array) $tag);
+        
+        $object->tagid = $newTag->_id;
         $f3->set('HALT', false) ;
        // $f3->set('DEBUG', 0) ;
         $result = array();
-
-
 
         try {
           $model->create((array) $object);
