4110af48523ce828df92034a326382df698fbb6b
Chris French <chris@ammonitenetworks.com>
2014-05-01 12:50:57 -0600

    adding dash files

---
+++ apps/Dash/Site/Views/event/wristbands/list.php
@@ -0,0 +1,116 @@
+<a class="btn btn-success pull-right" href="./<?php echo $PARAMS['eventid']?>/wristband/create">New wristbands</a>
+
+<br clear="both"><br>
+<form id="searchForm" action="" method="post">
+
+    <div class="row datatable-header">
+        <div class="col-sm-6">
+            <div class="row row-marginless">
+                <?php if (!empty($list->total_pages) && $list->total_pages > 1) { ?>
+                                <?php echo $list->serve(); ?>
+                 <?php } ?>
+                 <?php if (!empty($list->items)) { ?>
+                        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                            <span class="pagination">
+                            <?php echo $list->getLimitBox( $state->get('list.limit') ); ?>
+                            </span>
+                        </div>
+                        <?php } ?>
+
+            </div>
+        </div>    
+        <div class="col-sm-4 pull-right">
+            <div class="input-group">
+                <input class="form-control" type="text" name="filter[keyword]" placeholder="Keyword" maxlength="200" value="<?php echo $state->get('filter.keyword'); ?>"> 
+                <span class="input-group-btn">
+                    <input class="btn btn-primary" type="submit" onclick="this.form.submit();" value="Search" />
+                </span>
+            </div>
+        </div>
+    </div>
+    <br/><br/>
+    <input type="hidden" name="list[order]" value="<?php echo $state->get('list.order'); ?>" />
+    <input type="hidden" name="list[direction]" value="<?php echo $state->get('list.direction'); ?>" />
+    
+    <div class="table-responsive datatable">
+    
+    <table class="table table-striped table-bordered table-hover table-highlight table-checkable">
+        <thead>
+            <tr>
+              
+                <th data-sortable="metadata.title">Band URL</th>
+                <th>Created Date</th>
+                <th>State</th>
+                <th>Assigned</th>
+                
+                <th class="col-md-1"></th>
+            </tr>
+        </thead>
+        <tbody>    
+    
+        <?php if (!empty($list)) { ?>
+    
+        <?php foreach ($list->items as $item) { ?>
+
+            <tr>
+                
+                            
+                <td class="">
+                    <a href="./<?php echo $PARAMS['eventid'] ?>/wristband/edit/<?php echo $item->id; ?>">
+                    http://ryst.cc/b/<?php echo $item->{'tagid'}; ?>
+                    </a>             
+                </td>
+                
+                <td class="">
+                    <?php echo $item->{'last_name'}; ?>
+                </td>
+                
+                <td class="">
+                    <?php echo $item->{'phone'}; ?>
+                </td>
+                
+                <td class="">
+                    <?php echo $item->{'email'}; ?>
+                </td>
+                                
+                <td class="text-center">
+                    <a class="btn btn-s btn-success" href="./<?php echo $PARAMS['eventid'] ?>/wristband/edit/<?php echo $item->id; ?>">
+                        <i class="icon-edit"></i>
+                    </a>
+                    &nbsp;
+                   <!-- <a class="btn btn-s btn-danger" data-bootbox="confirm" href="./admin/blog/post/delete/<?php echo $item->id; ?>">
+                        <i class="icon-trash"></i>
+                    </a> -->
+                </td>
+            </tr>
+        <?php } ?>
+        
+        <?php } else { ?>
+            <tr>
+            <td colspan="100">
+                <div class="">No items found.</div>
+            </td> 
+            </tr>
+        <?php } ?>
+
+        </tbody>
+    </table>
+    
+    </div>
+    
+    <div class="row datatable-footer">
+         <div class="col-sm-10">
+                    <?php if (!empty($list->total_pages) && $list->total_pages > 1) { ?>
+                        <?php echo $list->serve(); ?>
+                    <?php } ?>
+                </div>
+                <div class="col-sm-2">
+                    <div class="datatable-results-count pull-right">
+                        <span class="pagination">
+                            <?php echo (!empty($list->total_pages)) ? $list->getResultsCounter() : null; ?>
+                        </span>
+                    </div>
+                </div>           
+    </div>
+
+</form>
