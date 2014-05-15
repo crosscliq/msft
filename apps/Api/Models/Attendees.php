8349c0850402c6a30a10178e6c8839dabbd1ec09
Chris French <chris@ammonitenetworks.com>
2014-05-15 09:28:58 -0400

    MSFT changes

--- apps/Api/Models/Attendees.php
+++ apps/Api/Models/Attendees.php
@@ -6,7 +6,6 @@ class Attendees extends \Msft\Models\Attendees
     public function getDb()
     {   
        $db_name = \Base::instance()->get('PARAMS.eventid');
-       
        \Base::instance()->set('db.mongo.server',\Base::instance()->get('db.mongo.server').'/'.$db_name);
        $db_server = \Base::instance()->get('db.mongo.server');
        return new \MongoDB( new \MongoClient($db_server), $db_name);;
