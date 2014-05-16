e77e33ceea7b944638b4906e25813cae63b0d29d
Chris French <chris@ammonitenetworks.com>
2014-05-16 12:06:04 -0400

    testing new sub stuff

--- apps/Msft/Userlistener.php
+++ apps/Msft/Userlistener.php
@@ -29,20 +29,16 @@ class Userlistener extends \Prefab
            
             $params = array( "userid" => 'msstore_nso', "password" => "msstore_nso", "keyword" => $event->{'sms.keyword'}, "acceptterms" => 1);              
              $params['phone'] = $model->phone;
+             $xml = '<datafields>';
+             $xml .= '<datafield id="106794">'.@$model->first_name.'</datafield> '; //First Name
+             $xml .= '<datafield id="106796">'.@$model->last_name.'</datafield> '; //Last Name
+             $xml .= '<datafield id="106792">'.@$model->zipcode.'</datafield> '; //Zip Code
+             $xml .= '<datafield id="106798">'.@$model->gender.'</datafield> '; //Gender
 
-             $params['datafield_xml'] = '<datafields>
-          <datafield id="106794">xyz</datafield> 
-          <datafield id="106796">xyz</datafield>
-          <datafield id="106792">xyz</datafield>
-          <datafield id="106798">abc</datafield>
-          </datafields>';
+             $params['datafield_xml'] = $xml ;
+        
             $response = $client->__soapCall("subscribe", $params);
-        /*       $response = $client->subscribe('msstore_nso', 'msstore_nso', $event->{'sms.keyword'}, 1, $model->phone, '<datafields>
-          <datafield id="106794">xyz</datafield> 
-          <datafield id="106796">xyz</datafield>
-          <datafield id="106792">xyz</datafield>
-          <datafield id="106798">abc</datafield>
-          </datafields>'); */
+        
               $model->set('smsdebug',$response );
                 $model->save();
 
@@ -52,6 +48,12 @@ class Userlistener extends \Prefab
               } else {
                 //notsure
               }
+          /*       $response = $client->subscribe('msstore_nso', 'msstore_nso', $event->{'sms.keyword'}, 1, $model->phone, '<datafields>
+          <datafield id="106794">xyz</datafield> 
+          <datafield id="106796">xyz</datafield>
+          <datafield id="106792">xyz</datafield>
+          <datafield id="106798">abc</datafield>
+          </datafields>'); */
         }
     }
 
