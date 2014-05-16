0ba248b0bfd02444314e1e6e3bad751ab1bb89b7
Chris French <chris@ammonitenetworks.com>
2014-05-16 13:39:13 -0400

    ok maybe working

--- apps/Msft/Userlistener.php
+++ apps/Msft/Userlistener.php
@@ -21,21 +21,19 @@ class Userlistener extends \Prefab
     }
 
     public function doSMSsub($model) {
-      
+
        if(strlen($model->phone) > 6 && $model->{'offers.sms'} == 'on' && empty($model->{'offers.smssubscribed'})) {
          $event = \Dsc\System::instance()->get('session')->get('event');
 	
             $client = new \SoapClient("https://www.cellitstudio.com/internal/webservice.php?wsdl");
            
              $xml = '<datafields>';
-             $xml .= '<datafield id="106794">'.@$model->first_name.'</datafield> '; //First Name
-             $xml .= '<datafield id="106796">'.@$model->last_name.'</datafield> '; //Last Name
-             $xml .= '<datafield id="106792">'.@$model->zipcode.'</datafield> '; //Zip Code
-             $xml .= '<datafield id="106798">'.@$model->gender.'</datafield> '; //Gender
+             $xml .= '<datafield id="106794">'.@$model->first_name.'</datafield>'; //First Name
+             $xml .= '<datafield id="106796">'.@$model->last_name.'</datafield>'; //Last Name
+             $xml .= '<datafield id="106792">'.@$model->zipcode.'</datafield>'; //Zip Code
+             $xml .= '<datafield id="106798">'.@$model->gender.'</datafield>'; //Gender
 	     $xml .= '</datafields>';	
-        var_dump($event);
-        var_dump( $xml);
-        die('doSMSsub');
+   
      
         $response = $client->subscribe_with_datafields('msstore_nso', 'msstore_nso',$model->phone,  $event->{'sms.keyword'}, 1, $xml);      
 	
