10a1858f0f69f492251cf3c0efcebbc262359ebf
Chris French <chris@ammonitenetworks.com>
2014-05-19 15:23:21 -0600

    better phone formatter

--- apps/Msft/Site/Views/games/raffle/play.php
+++ apps/Msft/Site/Views/games/raffle/play.php
@@ -1,4 +1,33 @@
 <?php 
+function formatPhoneNumber($s) {
+$rx = "/
+    (1)?\D*     # optional country code
+    (\d{3})?\D* # optional area code
+    (\d{3})\D*  # first three
+    (\d{4})     # last four
+    (?:\D+|$)   # extension delimiter or EOL
+    (\d*)       # optional extension
+/x";
+preg_match($rx, $s, $matches);
+if(!isset($matches[0])) return false;
+
+$country = $matches[1];
+$area = $matches[2];
+$three = $matches[3];
+$four = $matches[4];
+$ext = $matches[5];
+
+$out = "$three-$four";
+if(!empty($area)) $out = "($area) $out";
+//if(!empty($country)) $out = "+$country$out";
+//if(!empty($ext)) $out .= "x$ext";
+
+// check that no digits were truncated
+// if (preg_replace('/\D/', '', $s) != preg_replace('/\D/', '', $out)) return false;
+return $out;
+}
+
+
 
 function format_telephone($phone_number)
 {
@@ -25,7 +54,7 @@ function format_telephone($phone_number)
                 <div id="role" style="text-align:center;">
 			
 			<h2 class="fg-white"><?php echo $game["winner"]["first_name"]; ?> <?php echo $game["winner"]["last_name"]; ?></h2>
-			<br/>PHONE: <?php echo format_telephone($game["winner"]["phone"]); ?><br/>
+			<br/>PHONE: <?php echo formatPhoneNumber($game["winner"]["phone"]); ?><br/>
 			<br/>Registered: <?php echo $game["winner"]["created"]["local"]; ?><br/>
 <form action="/games/raffle/play" method="post">
 <button type="submit"  class="button large inverse fg-white" style="width:80%; margin-bottom:25px;background:rgba(0,0,0,0.6)!important">Play Again</button>
